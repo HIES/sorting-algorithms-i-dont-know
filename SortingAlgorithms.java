8fdc093c0ae46dff6a6c02e613a54fb63af71341
NuclearWinter22 <wilsonja@bearsmail.org>
2017-10-17 07:38:50 -0400

    Class 1 Comit

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,10 +1,38 @@
 public class SortingAlgorithms
 {
     public static void bubbleSort(int[] nums)
-    {}
+    {
+        int counter = 1;
+        for (int i = 0; i < nums.length; i++) 
+        {
+            for(int x = 0; x < nums.length-counter; x++) 
+            {
+                if (nums[x] > nums[x+1]) 
+                {
+                    int temp = nums[x+1];
+                    nums[x+1] = nums[x];
+                    nums[x] = temp;                  
+                }
+            }
+            counter++;
+        }
+    }
 
-    public static void selectionSort(int[] nums)
-    {}
+    public static void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length-1; i++) {
+            int min = nums[i];
+            int minIndex = i;
+            for (int y = i; y < nums.length; y++) {
+                if (nums[y] < min) {
+                    min = nums[y];
+                    minIndex = y;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = min;
+            nums[minIndex] = temp;
+        }
+    }
 
     public static void insertionSort(int[] nums)
     {}
@@ -53,19 +81,20 @@ public class SortingAlgorithms
     // Use main for testing and timing your sorts
     public static void main(String[] args) 
     {
-        int[] testArray = generateReversedArray(1000);
+        int[] testArray = generateRandomArray(5000);
 
-        //printArray(testArray);
+        printArray(testArray);
 
         System.out.println("Applying the bubble sort.");
 
         long start = System.currentTimeMillis();
-        bubbleSort(testArray);
+        //bubbleSort(testArray);
+        selectionSort(testArray);
         long end = System.currentTimeMillis();
         double sortTime = (end - start) / 1000.0; 
 
-        //printArray(testArray);
-
+        printArray(testArray);
+        System.out.println(isSorted(testArray));
         System.out.format("Time to complete took %.4f seconds.%n", sortTime);
     }
 }
